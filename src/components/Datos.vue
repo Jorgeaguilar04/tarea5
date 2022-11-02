0015818ce1d8f03c970c6840e081f66a19f99487
JORGE AGUILAR GUTIERREZ <94917439+Jorgeaguilar04@users.noreply.github.com>
2022-11-01 23:23:58 -0300

    Add files via upload

---
+++ src/components/Datos.vue
@@ -0,0 +1,34 @@
+<template>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <div class="table-responsive">
+ 
+               <table class="table">
+                  <thead class="thead-dark">
+                     <tr>
+                        <th scope="col">name</th>
+                        <th scope="col">name</th>
+                        <th scope="col">name</th>
+                        <th scope="col">location</th>
+                        <th scope="col">gender</th>
+                        <th scope="col">imagen</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <tr v-for="item in items" v-bind:key="item.id">
+                         <td>{{ item.name.tittle }}</td>
+                        <td>{{ item.name.first }}</td>
+                         <td>{{ item.name.last }}</td>
+                        <td>{{ item.location.country }}</td>
+                        <td>{{ item.gender }}</td>
+                        <td><img v-bind:src="'img/' + item.img" class="img-fluid" width="30px" v-bind:alt="item.nombre"></td>
+                     </tr>
+                  </tbody>
+               </table>
+ 
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
